404128383fa957572942e8482cc80fa1c8734fdb
SouvikGhorui <sghorui29@gmail.com>
2026-03-22 02:00:34 +0530

    Add Level order traversal

--- Binary Tree/IntroBinaryTree.cpp
+++ Binary Tree/IntroBinaryTree.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 class Node{
@@ -26,6 +27,7 @@ Node* buildTree(vector<int> preorder ){
 
 }
 
+//preorder traversal
 void preOrder(Node* root){
     if( root == NULL) return;
     cout<<root->data<<endl;
@@ -33,11 +35,46 @@ void preOrder(Node* root){
     preOrder(root->right);
 }
 
+//inorder traversal
+void inOrder(Node* root){
+    if (root == NULL) return;
+    inOrder(root->left);
+    cout<< root->data<<endl;
+    inOrder(root->right);
+}
+
+//postorder traversal
+void postOrder(Node* root){
+    if(root == NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<< endl;
+}
+
+
+//level order
+void levelOrder(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    while(q.size()>0){
+        Node* curr = q.front();
+        q.pop();
+
+        cout<<curr->data<< " ";
+        if (curr->left != NULL){
+            q.push(curr->left);
+        }
+        if (curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+}
+
+
 int main(){
     vector<int> preorder = {1 ,2 ,-1 ,-1 , 3 , 4, -1, -1, 5, -1, -1};
     Node* root = buildTree(preorder);
-    preOrder(root);
-    
-
+    levelOrder(root);
     return 0;
 }
