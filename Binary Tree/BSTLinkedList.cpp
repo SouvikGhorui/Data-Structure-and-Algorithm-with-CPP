dab36e0d9b2b38a434fa69cbb3a9067eb4928abf
SouvikGhorui <sghorui29@gmail.com>
2026-03-30 00:15:55 +0530

    add insert usng recursion and iteration

--- Binary Tree/BSTLinkedList.cpp
+++ Binary Tree/BSTLinkedList.cpp
@@ -1,62 +1,59 @@
 #include<iostream>
 using namespace std;
-
-class treeNode{
+class Node{
 public:
     int data;
-    treeNode* left;
-    treeNode* right;
-    treeNode(int val){
+    Node* left;
+    Node* right;
+    Node(int val){
         data = val;
-        left = NULL;
-        right = NULL;
+        left = right = NULL;
     }
 };
 
 class BST{
 public:
-    treeNode* root;
+    Node* root;
     BST(){
         root = NULL;
     }
-    bool isEmpty(){
-        return root == NULL;
-    }
-};
-int main(){
-    int option;
 
-    do{
-        cout<<"What operation do you want to perform"<<"Select option numbers"
-        <<"Press 0 to exit"<<endl;
-        cout<<"1. Insert Node"<< endl;
-        cout<<"2. Search Node"<< endl;
-        cout<<"3. Delete Node"<< endl;
-        cout<<"4. Print BST Values"<< endl;
-        cout<<"0. Exit program"<< endl;
     
-        cin>>option;
-    switch(option){
-        case 0:
-            break;
-        case 1:
-            cout<<"Insert"<<endl;
-            break;
-        case 2:
-            cout<<"Search"<<endl;
-            break;
-        case 3:
-            cout<<"Delete"<<endl;
-            break;
-        case 4:
-            cout<<"Print an traverse"<<endl;
-            break;
-        default:
-            cout<<"Enter valid number"<<endl;
-            break;
-        
-    };
-    }while(option!=0);
+    Node* insert(Node* root, int val){
+        Node* newNode = new Node(val);
+        if(root == NULL) return newNode;
+        Node* curr = root;
+        while(curr!= NULL){
+            if(curr->data > val && curr->left != NULL){
+                curr = curr->left;
+            }
+            else if(curr ->data < val && curr->right != NULL){
+                curr = curr->right;
+            }
+            else break;
+        }
+        if(curr->data > val){
+            curr->left = newNode;
+        }
+        else{
+            curr -> right = newNode;
+        }
+        return root;
+    }
 
-    return 0;
-}
+    Node* insertRecursion( Node* root, int val){
+        Node* newNode = new Node(val);
+        if(root == NULL){
+            return newNode;
+        }
+        if(root->data > val){
+            insertRecursion(root->left, val);
+        }
+        else{
+            insertRecursion(root->right, val);
+        }
+        return root;
+    }
+
+    
+};
